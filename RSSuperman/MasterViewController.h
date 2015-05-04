21e60adaaf0c0de94a4991bd602247895f96a56b
4pcbr <me@4pcbr.com>
2015-05-04 23:28:04 +0200

    Initial Commit

---
+++ RSSuperman/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  RSSuperman
+//
+//  Created by Olegs on 04/05/15.
+//  Copyright (c) 2015 Brand New Heroes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ RSSuperman/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RSSuperman
+//
+//  Created by Olegs on 04/05/15.
+//  Copyright (c) 2015 Brand New Heroes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
